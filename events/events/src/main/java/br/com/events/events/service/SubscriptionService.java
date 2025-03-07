d5a2d7041fbeca06c6f842cff5b8bb807f8e1e23
Zenx007 <zenxpokas@gmail.com>
2025-03-06 23:59:10 -0300

    update

--- events/events/src/main/java/br/com/events/events/service/SubscriptionService.java
+++ events/events/src/main/java/br/com/events/events/service/SubscriptionService.java
@@ -3,6 +3,8 @@ package br.com.events.events.service;
 import br.com.events.events.model.Subscription;
 import br.com.events.events.model.User;
 import br.com.events.events.repo.EventRepo;
+import br.com.events.events.repo.SubscriptionRepo;
+import br.com.events.events.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,12 @@ public class SubscriptionService {
 
     @Autowired
     private EventRepo evtRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private SubscriptionRepo subRepo;
 
     public Subscription createNewSubscription(String eventName, User user) {
         Subscription subs = new Subscription();
