2cea527fa3ca609607bbbe167bf100326af97fc7
Zenx007 <zenxpokas@gmail.com>
2025-03-09 17:12:43 -0300

    Update

--- events/events/src/main/java/br/com/events/events/controller/SubscriptionController.java
+++ events/events/src/main/java/br/com/events/events/controller/SubscriptionController.java
@@ -17,10 +17,14 @@ public class SubscriptionController {
     private SubscriptionService service;
 
     @PostMapping("/subscription/{prettyName}")
-    public ResponseEntity<Subscription> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
-        Subscription res = service.createNewSubscription(prettyName, subscriber);
-        if (res != null) {
-            return ResponseEntity.ok(res);
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
+        try {
+            Subscription res = service.createNewSubscription(prettyName, subscriber);
+            if (res != null) {
+                return ResponseEntity.ok(res);
+            }
+        } catch(EventNotFoundException ex) {
+            return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         }
         return ResponseEntity.badRequest().build();
     }
