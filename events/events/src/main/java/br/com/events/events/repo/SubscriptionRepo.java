604754f0992ec6f97d43cb21cf05e6e514b232ff
Zenx007 <zenxpokas@gmail.com>
2025-03-10 17:50:37 -0300

    Update

--- events/events/src/main/java/br/com/events/events/repo/SubscriptionRepo.java
+++ events/events/src/main/java/br/com/events/events/repo/SubscriptionRepo.java
@@ -1,8 +1,11 @@
 package br.com.events.events.repo;
 
+import br.com.events.events.model.Event;
 import br.com.events.events.model.Subscription;
 
+import br.com.events.events.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SubscriptionRepo extends CrudRepository<Subscription, Integer> {
+    public Subscription findByEventAndSubscriber(Event evt, User user);
 }
