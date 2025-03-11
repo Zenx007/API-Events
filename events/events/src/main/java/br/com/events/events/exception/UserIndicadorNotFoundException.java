ddc93fe24d2d93a85d31aa6d23936a63332fa32e
Zenx007 <zenxpokas@gmail.com>
2025-03-11 17:19:38 -0300

    Update

---
+++ events/events/src/main/java/br/com/events/events/exception/UserIndicadorNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.events.events.exception;
+
+public class UserIndicadorNotFoundException extends RuntimeException {
+    public UserIndicadorNotFoundException(String msg) {
+        super(msg);
+    }
+}
