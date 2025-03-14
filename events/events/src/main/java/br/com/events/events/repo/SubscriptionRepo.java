2d388ba0ce060d08fa0bf5ecd82fdae69b7b66c9
Zenx007 <zenxpokas@gmail.com>
2025-03-14 16:16:05 -0300

    Update

--- events/events/src/main/java/br/com/events/events/repo/SubscriptionRepo.java
+++ events/events/src/main/java/br/com/events/events/repo/SubscriptionRepo.java
@@ -14,13 +14,13 @@ import java.util.List;
 public interface SubscriptionRepo extends CrudRepository<Subscription, Integer> {
     public Subscription findByEventAndSubscriber(Event evt, User user);
 
-    @Query(value = " select count (subscription_number as quantidade, indication_user_id,user_name"
+    @Query(value = " select count(subscription_number) as quantidade, indication_user_id, user_name"
     +" from tbl_subscription inner join tbl_user"
-    + " on tbl_subscription.indication_user_id = tbl_user"
+    + " on tbl_subscription.indication_user_id = tbl_user.user_id"
     +" where indication_user_id is not null "
     +"  and event_id = :eventId"
     + " group by indication_user_id"
-    +" order by quantidade desc;" nativeQuery = true)
+    +" order by quantidade desc", nativeQuery = true)
 
 
     public List<SubscriptionRankingItem> generateRanking(@Param("eventId") Integer eventId);
