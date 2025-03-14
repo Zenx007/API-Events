5aa9cf4faca302caa5d1bd141f68b3a56a999c4b
Zenx007 <zenxpokas@gmail.com>
2025-03-13 22:56:46 -0300

    Update

--- events/events/src/main/java/br/com/events/events/repo/SubscriptionRepo.java
+++ events/events/src/main/java/br/com/events/events/repo/SubscriptionRepo.java
@@ -8,4 +8,15 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface SubscriptionRepo extends CrudRepository<Subscription, Integer> {
     public Subscription findByEventAndSubscriber(Event evt, User user);
+
+    @Query(value = " select count (subscription_number as quantidade, indication_user_id,user_name"
+    +" from tbl_subscription inner join tbl_user"
+    + " on tbl_subscription.indication_user_id = tbl_user"
+    +" where indication_user_id is not null "
+    +"  and event_id = :eventId"
+    + " group by indication_user_id"
+    +" order by quantidade desc;" nativeQuery = true)
+
+
+    public List<SubscriptionRankingItem> generateRanking(@Param("eventId") Integer eventId);
 }
